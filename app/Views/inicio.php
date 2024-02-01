0377f3bdfec36240cb9f5a9f4a3c987d8c578ea4
Jaddis02 <ccd0010@alu.medac.es>
2024-02-01 13:36:34 +0100

    cambios

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -99,27 +99,6 @@
         </div>
     </div>
 
-    <div class="col-12 col-md-9 m-0 p-4">
-        <div class="card shadow">
-            <img src="https://static.emol.cl/emol50/Fotos/2023/08/31/file_20230831172910.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Chile</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-            </div>
-        </div>
-    </div>
-
-    <div class="col-12 col-md-9 m-0 p-4">
-        <div class="card shadow">
-            <img src="https://blog.universaldeidiomas.com/wp-content/uploads/2017/06/Principales-sitios-turisticos-de-Barsil.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Brasil</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
-            </div>
-        </div>
-    </div>
 
 </div>
 </div>
